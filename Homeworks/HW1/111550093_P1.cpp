22a8fc54a814af589b9380a82dd0416752ee0044
ChuEating1005 <itingchu1005@gmail.com>
2024-09-20 04:15:30 +0800

    hw1: finish

--- Homeworks/HW1/111550093_P1.cpp
+++ Homeworks/HW1/111550093_P1.cpp
@@ -11,14 +11,130 @@ Statement: I am fully aware that this program is not supposed to be posted to a 
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <signal.h>
 
-#define MAX_TOKENS 100
+#define BUFFER 100
+#define EXIT 0
+#define NORMAL 1
+#define INPUT_FILE 2
+#define OUTPUT_FILE 3
+#define PIPE 4
+#define ZOMBIE 5
 
 using namespace std;
 
+int status_check(char input[BUFFER]) {
+    if(strcmp(input,"exit") == 0 || strcmp(input,"exit &") == 0){
+        return EXIT;
+    }
+    else if(strchr(input, '<') != NULL){
+        return INPUT_FILE;
+    }
+    else if(strchr(input, '>') != NULL){
+        return OUTPUT_FILE;
+    }
+    else if(strchr(input, '|') != NULL){
+        return PIPE;
+    }
+    else if(strchr(input, '&') != NULL){
+        return ZOMBIE;
+    }
+    else{
+        return NORMAL;
+    }
+} 
 
-void split_command(char *cmd, char **lcmd, char **rcmd, bool &pipe, bool &wait_child) {
-    char *tokens[MAX_TOKENS];
+void sigchld_handler(int sig) {
+    while (waitpid(-1, NULL, WNOHANG) > 0) {}
+}
+
+void piping(char **lcmd, char **rcmd) {
+    int fd[2];
+    pid_t pid1, pid2;
+
+    if (pipe(fd) == -1) {
+        perror("Pipe failed");
+        exit(1);
+    }
+    
+    pid1 = fork();
+    if (pid1 == 0) {  // Child process 1 (executes left command, e.g., `ls`)
+        close(fd[0]); // Close the read end of the pipe
+        dup2(fd[1], STDOUT_FILENO);  // Redirect standard output to the write end of the pipe
+        close(fd[1]); // Close the write end of the pipe
+        execvp(lcmd[0], lcmd);  // Execute left command
+        perror("execvp failed");
+        exit(1);
+    }
+    
+    pid2 = fork();
+    if (pid2 == 0) {  // Child process 2 (executes right command, e.g., `more`)
+        close(fd[1]);   // Close the write end of the pipe
+        dup2(fd[0], STDIN_FILENO);  // Redirect standard input to the read end of the pipe
+        close(fd[0]);   // Close the read end of the pipe
+        execvp(rcmd[0], rcmd);  // Execute right command
+        perror("execvp failed");
+        exit(1);
+    }
+
+    // Parent process (wait for child processes)
+    close(fd[0]);
+    close(fd[1]); // Parent doesn't need the pipe, close it
+    waitpid(pid1, NULL, 0);  // Wait for the left-side child process to finish
+    waitpid(pid2, NULL, 0);  // Wait for the right-side child process to finish
+}
+
+void redirect_stdio(int status, char **lcmd, char **rcmd, char **file) {
+    pid_t pid;
+    pid = fork();
+    if (pid < 0) { /* error occurred */
+        fprintf(stderr, "Fork Failed");
+        exit(-1);
+    }
+    else if (pid == 0) { /* child process */
+        int fd;
+        if (status == INPUT_FILE){
+            // Redirect inupt
+            fd = open(*file, O_RDONLY, S_IRUSR | S_IWUSR);
+            dup2(fd, STDIN_FILENO);
+        }
+        else if (status == OUTPUT_FILE) {
+            // Redirect output
+            fd = open(*file, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+            dup2(fd, STDOUT_FILENO);
+        }
+        close(fd);
+        execvp(lcmd[0], lcmd);
+        exit(1);
+    }
+    else { /* parent process */
+        waitpid(pid, NULL, 0);
+    }
+}
+
+void execute_cmd(char **cmd, bool zombie) {
+    pid_t  pid = fork();
+    if (pid < 0) { /* error occurred */
+        fprintf(stderr, "Fork Failed");
+        exit(-1);
+    }
+    else if (pid == 0) { /* child process */
+        execvp(cmd[0], cmd);
+        exit(1);
+    }
+    else { /* parent process */
+        if(zombie) {
+            waitpid(-1, NULL, WNOHANG);
+        }
+        else {
+            waitpid(pid, NULL, 0);
+        }
+    }
+}
+
+void split_command(char *cmd, char **lcmd, char **rcmd, char **file) {
+    char *tokens[BUFFER];
+    bool pipe_flag = false;
     int num_tok = 0;
 
     char *token = strtok(cmd, " ");
@@ -27,70 +143,69 @@ void split_command(char *cmd, char **lcmd, char **rcmd, bool &pipe, bool &wait_c
         token = strtok(NULL, " ");
     }
 
-    char *last1 = NULL, *last2 = NULL;
+    int lidx = 0, ridx = 0;
     for (int j = 0; j < num_tok; j++) {
         // Check if the token is a pipe
         if (strcmp(tokens[j], "|") == 0) {
-            pipe = true;
+            pipe_flag = true;
             continue;
         }
-        else if (strcmp(tokens[j], "&") == 0) {
-            wait_child = true;
+        else if (strcmp(tokens[j], ">") == 0 || strcmp(tokens[j], "<") == 0) {
+            *file = tokens[j+1];
+            lcmd[lidx] = NULL;
             break;
         }
 
-        if (!pipe) {
-            *lcmd++ = tokens[j];
-            last1 = tokens[j];
-        } 
-        else {
-            *rcmd++ = tokens[j];
-            last2 = tokens[j];
-        }
-
-        if (strcmp(last1, "&") == 0) {
-            wait_child = false;
-            *lcmd--;
-            break;
-        }
-        *lcmd = NULL;
-
-        if (pipe) {
-            if (strcmp(last2, "&") == 0) {
-                wait_child = false;
-                *rcmd--;
+        if (!pipe_flag) {
+            if (strcmp(tokens[j], "&") == 0) {
+                lcmd[lidx] = NULL;
                 break;
             }
-            *rcmd = NULL;
+            lcmd[lidx++] = tokens[j];
+        } 
+        else {
+            rcmd[ridx++] = tokens[j];
         }
     }
+
+    lcmd[lidx] = NULL;
+    rcmd[ridx] = NULL;
 }
 
 
 int main() {
+    char input[BUFFER];
 
-    pid_t  pid;
+    signal(SIGCHLD, sigchld_handler); // Handle zombie processes with SIGCHLD
+    
+    printf("> ");
+    while(fgets(input, BUFFER, stdin) != NULL){        
+        input[strlen(input) - 1] = '\0';
+        int status = status_check(input);
+        if (status == EXIT) break;
 
-    printf(">");
-    char input[100];
+        char *lcmd[BUFFER], *rcmd[BUFFER], *file = NULL;
+        split_command(input, lcmd, rcmd, &file);
 
-    while(fgets(input, 100, stdin) != NULL){
-        char *lcmd[MAX_TOKENS], *rcmd[MAX_TOKENS];
-        bool pipe = false, wait_child = true;
-        split_command(input, lcmd, rcmd, pipe, wait_child);
-        
-        pid = fork();
-
-        if (pid < 0) { /* error occurred */
-            fprintf(stderr, "Fork Failed");
-            exit(-1);
+        switch(status){
+            case PIPE: {
+                piping(lcmd, rcmd);
+                break;
+            }
+            case INPUT_FILE:
+            case OUTPUT_FILE: {
+                redirect_stdio(status, lcmd, rcmd, &file);
+                break;
+            }
+            case ZOMBIE: {
+                execute_cmd(lcmd, true);
+                break;
+            }
+            case NORMAL: {
+                execute_cmd(lcmd, false);
+                break;
+            }
         }
-        else if (pid == 0) { /* child process */
-            execvp(lcmd[0], lcmd);
-        }
-        else { /* parent process */
-            /* parent will wait for the child to complete */
-            wait (NULL);
-        }
+        printf("> ");
     }
 }
