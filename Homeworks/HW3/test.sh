6aa081a0a6b85c6b18be1f031fae40bf37346361
Chu I-Ting <itingchu1005@gmail.com>
2024-10-13 14:08:01 +0800

    hw3: modify cpp filename

--- Homeworks/HW3/test.sh
+++ Homeworks/HW3/test.sh
@@ -2,7 +2,7 @@
 
 python3 test.py
 echo "Testcase generated"
-g++ -pthread 111550093_P3.cpp -o hw3.out
+g++ -pthread hw3_111550093.cpp -o hw3.out
 echo "Compiled"
 echo ""
 output=$(./hw3.out | tee /dev/tty)
