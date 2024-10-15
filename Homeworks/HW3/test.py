319c6a3e17624bbc70a2b3d5ccaaeb8232aa5f44
ChuEating1005 <itingchu1005@gmail.com>
2024-10-15 15:58:43 +0800

    add helper function to evaluate correctness of output

--- Homeworks/HW3/test.py
+++ Homeworks/HW3/test.py
@@ -1,7 +1,13 @@
+import argparse
 from random import randint
 
-N = int(300000)
+# Add argument parser
+parser = argparse.ArgumentParser(description='Generate random numbers and write to a file.')
+parser.add_argument('-N', type=int, default=100000, help='Number of random numbers to generate')
+args = parser.parse_args()
+
+N = args.N
 with open('input.txt', 'w') as f:
-    f.write(f'{N} \n')
+    f.write(f'{N}\n')
     for i in range(N):
         f.write(f'{randint(-2147483648, 2147483647)} ')
